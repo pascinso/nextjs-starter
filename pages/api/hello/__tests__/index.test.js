d36d393ce605784e644d9db28ce76109847be0c3
Ebuks <ebukaa@protonmail.com>
2021-03-22 17:08:04 +0000

    made simplier boilerplate

--- pages/api/hello/__tests__/index.test.js
+++ pages/api/hello/__tests__/index.test.js
@@ -6,15 +6,19 @@ describe("useApi", () => {
     json: jest.fn(),
   };
 
-  global.fetch = () => ({
-    json: jest.fn(() => Promise.resolve()),
-  });
-
   describe("response", () => {
+    const json = jest.fn();
+    global.fetch = jest.fn();
+
     test("calls json", async () => {
+      jest.spyOn(global, "fetch").mockResolvedValueOnce({ json });
+
       await useApi(null, response);
+
       expect(response.json).toHaveBeenCalledTimes(1);
       expect(typeof response.statusCode).toEqual("number");
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(json).toHaveBeenCalledTimes(1);
     });
   });
 });
