423580129f44c87828d5c293740cad3afd9f3d76
Ebuks <ebukaa@protonmail.com>
2021-03-22 17:14:16 +0000

    fixed api tests

--- pages/api/hello/__tests__/index.test.js
+++ pages/api/hello/__tests__/index.test.js
@@ -7,18 +7,11 @@ describe("useApi", () => {
   };
 
   describe("response", () => {
-    const json = jest.fn();
-    global.fetch = jest.fn();
-
     test("calls json", async () => {
-      jest.spyOn(global, "fetch").mockResolvedValueOnce({ json });
-
       await useApi(null, response);
 
       expect(response.json).toHaveBeenCalledTimes(1);
       expect(typeof response.statusCode).toEqual("number");
-      expect(global.fetch).toHaveBeenCalledTimes(1);
-      expect(json).toHaveBeenCalledTimes(1);
     });
   });
 });
