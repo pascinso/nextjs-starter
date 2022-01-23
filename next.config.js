5cc5df27f6a4b1f33d903562bf78a5bc64c40e7d
Ebuks <ebukaa@protonmail.com>
2022-01-23 18:33:29 +0000

    using SWR instead of babel and now standalone

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   distDir: "build",
   pageExtensions: ["page.jsx", "page.js"],
-  target: "serverless",
+  outputStandalone: true,
   reactStrictMode: true,
   webpack(config) {
     config.resolve.modules.push(__dirname);
