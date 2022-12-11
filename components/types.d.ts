31653e67d21bb1ef5d35d08f61a07afdd282bf7b
Ebuks <ebukaa@protonmail.com>
2022-12-11 11:34:54 +0000

    Added, updated & exported types

--- components/types.d.ts
+++ components/types.d.ts
@@ -1,10 +1,18 @@
 export type ArrayElement<ArrayType extends readonly unknown[]> =
   ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
 
-type Properties<ObjectType extends object> = {
-  [property in keyof ObjectType]: {
-    value: ObjectType[property];
+export type MakeOptional<Type, Key extends keyof Type> = Omit<Type, Key> &
+  Partial<Pick<Type, Key>>;
+
+export type Action = "get" | "set" | "access";
+
+export type Properties<ObjectType extends object> = {
+  [property in keyof MakeOptional<ObjectType, keyof ObjectType>]: {
+    value?: ObjectType[property];
     writable?: boolean;
+    get?: () => ObjectType[property];
+    set?: (param: ObjectType[property]) => void;
+    configurable?: boolean;
   };
 };
 
