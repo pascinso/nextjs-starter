99a5ff06337251a1f08f09c25d0b03a25e8ce422
Ebuka Isiadinso <39866607+pascinso@users.noreply.github.com>
2022-12-20 14:05:16 +0000

    Replaced Optional with Partial type & export ComponentType

--- components/types.d.ts
+++ components/types.d.ts
@@ -1,13 +1,10 @@
 export type ArrayElement<ArrayType extends readonly unknown[]> =
   ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
 
-export type Optional<Type, Key extends keyof Type> = Omit<Type, Key> &
-  Partial<Pick<Type, Key>>;
-
 export type Action = "get" | "set" | "access";
 
 export type Properties<Component extends object> = {
-  [property in keyof Optional<Component, keyof Component>]: {
+  [property in keyof Partial<Component>]: {
     value?: Component[property];
     writable?: boolean;
     get?: () => Component[property];
@@ -16,7 +13,7 @@ export type Properties<Component extends object> = {
   };
 };
 
-type ComponentType = object & { name: string };
+export type ComponentType = object & { name: string };
 
 interface ImportedComponent<Props extends object> {
   default(props: Props | object): JSX.Element | null;
