3cb88426fe1624bbe1d75354983a67d1c0a63975
Ebuks <ebukaa@protonmail.com>
2022-12-23 13:42:40 +0000

    Imported load from @pascinso/utils

--- pages/icon/index.ts
+++ pages/icon/index.ts
@@ -1,4 +1,4 @@
-import load from "components/utils";
+import { load } from "@pascinso/utils";
 
 export default function useVercel(props: VercelProps) {
   return load(import("public/vercel.svg"), props);
